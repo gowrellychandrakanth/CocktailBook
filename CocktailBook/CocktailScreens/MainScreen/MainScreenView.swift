0311bc63c395a15913802103850c0c50b3371a53
Chandra Kanth <chandrakanth.developer@gmail.com>
2025-03-07 19:21:05 +0530

    Scene Delegate Updated the window rootViewController with MainScreenView

---
+++ CocktailBook/CocktailScreens/MainScreen/MainScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenView.swift
+//  CocktailBook
+//
+//  Created by Chandra Kanth on 07/03/25.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreenView()
+}
